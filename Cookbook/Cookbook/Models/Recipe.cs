25a92c1efc752db16c73b917d74fe77bd5211b76
Amy <e.brudzisz@gmail.com>
2018-05-31 22:57:37 +0200

    Added Recipe model

---
+++ Cookbook/Cookbook/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Cookbook.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Difficulty { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
