223ba8ae22d51cc7d184eb5d2d680301f6b65a77
Amy <e.brudzisz@gmail.com>
2018-06-01 11:16:42 +0200

    Added Cake package

---
+++ Cookbook/Cookbook.Tests/Controllers/RecipesControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cookbook;
+using Cookbook.Controllers;
+using System.Web.Mvc;
+
+namespace Cookbook.Tests.Controllers
+{
+    [TestClass]
+    public class RecipesControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            RecipesController controller = new RecipesController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        
+    }
+}
